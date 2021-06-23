61a9352df0f32421b11aca5373399044677d4911
Rodrigo <rodrigo.ti@grupointegrado.br>
2021-06-22 23:42:30 -0300

    env

--- app.js
+++ app.js
@@ -34,15 +34,15 @@ app.use('/respostaQuestionario', respostaQuestionario)
 app.use('/usuario', usuario)
 
 //-----------------
-app.set('port', port)
-app.get('/', function (request, response) {
-  var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function () {
-  console.log('App is running, server is listening on port ', app.get('port'));
-});
+// app.set('port', port)
+// app.get('/', function (request, response) {
+//   var result = 'App is running'
+//   response.send(result);
+// }).listen(app.get('port'), function () {
+//   console.log('App is running, server is listening on port ', app.get('port'));
+// });
 //----------------
-
+app.listen(process.env.PORT || 5000)
 // app.listen(port, () => {
 //   console.log(`Running in http://localhost:${port}`);
 // })
